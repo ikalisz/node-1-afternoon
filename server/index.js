6fb01d8bbdb00d57de0f3de8ffea911cd71070d0
Ian Kaliszewski <ikalisz@yahoo.com>
2019-05-03 14:00:09 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -3,11 +3,35 @@ const app = express()
 const products = require('../products.json')
 const getProducts = require('./getProducts')
 
-app.get('/api/products', getProducts)
-
 app.get('/api/products/:id', getProducts)
 
+app.get(`/api/products`, (req, res) => {
+    if (req.query.price) {
+        let filtered = products.filter((val) => {
+            val.price > req.query.price
+        })
+        res.status(200).send(filtered)
+    } else {
+        res.status(200).send(products)
+    }
+})
 
-app.listen(3000, () => {
+// axios.get('/api/products/:id?name=name, {body})
+
+// query = {
+//     name: 'Tayte'
+// }
+
+// body = {
+//     age: 21,
+//     gender: male
+// }
+
+// params = {
+//     id: 8
+// }
+
+
+app.listen(3001, () => {
     console.log('Working!')
 })
